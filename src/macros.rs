e12effa1b19c84eeef987f9db34319415790080b
Tim Satke <tim.satke@gmail.com>
2022-03-02 20:57:25 +0100

    remove commented code

--- src/macros.rs
+++ src/macros.rs
@@ -1,32 +1,3 @@
-// adapted from the rust std crate
-// #[macro_export]
-// macro_rules! dbg {
-//     () => {
-//         #[cfg(debug_assertions)]
-//         $crate::serial_println!("[{}:{}]", file!(), line!())
-//     };
-//     ($val:expr) => {
-//         #[cfg(debug_assertions)]
-//         $crate::serial_println!(concat!("[{}:{}] ", $val), file!(), line!());
-//     };
-//     ($val:expr $(,)?) => {
-//         // Use of `match` here is intentional because it affects the lifetimes
-//         // of temporaries - https://stackoverflow.com/a/48732525/1063961
-//         #[cfg(debug_assertions)]
-//         match $val {
-//             tmp => {
-//                 $crate::serial_println!("[{}:{}] {} = {:#?}",
-//                     file!(), line!(), stringify!($val), &tmp);
-//                 tmp
-//             }
-//         }
-//     };
-//     ($($val:expr),+ $(,)?) => {
-//         #[cfg(debug_assertions)]
-//         ($($crate::dbg!($val)),+,)
-//     };
-// }
-
 #[macro_export]
 macro_rules! info {
     ($fmt:expr) => ($crate::serial_print!(concat!("[{}] ", $fmt, "\n"), module_path!()));
